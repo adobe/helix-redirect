db2b75f1a558c2571b19466b5e6862be0bd17f99
Lars Trieloff <trieloff@adobe.com>
2020-06-26 10:34:42 +0000

    test(post-deploy): add post-deploy test

--- test/post-deploy.js
+++ test/post-deploy.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Adobe. All rights reserved.
+ * Copyright 2020 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -11,13 +11,55 @@
  */
 
 /* eslint-env mocha */
-const assert = require('assert');
+/* eslint-disable no-unused-expressions */
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const packjson = require('../package.json');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+function getbaseurl() {
+  const namespace = 'helix';
+  const package = 'helix-services';
+  const name = packjson.name.replace('@adobe/helix-', '');
+  let version = `${packjson.version}`;
+  if (process.env.CI && process.env.CIRCLE_BUILD_NUM && process.env.CIRCLE_BRANCH !== 'master') {
+    version = `ci${process.env.CIRCLE_BUILD_NUM}`;
+  }
+  return `api/v1/web/${namespace}/${package}/${name}@${version}`;
+}
 
 describe('Post-Deploy Tests', () => {
-  it('Service is ready for monitoring', () => {
-    assert.equal(
-      'Not yet, but I will change this line as soon as I am ready',
-      'I am ready to go on call for this',
-    );
-  });
+  it('301 Redirect', async () => {
+    const qs = '?owner=trieloff&repo=helix-demo&ref=4e05a4e2c7aac6dd8d5f2b6dcf05815994812d7d&path=test.php';
+    // eslint-disable-next-line no-console
+    console.log(`Trying https://adobeioruntime.net/${getbaseurl()}${qs}`);
+
+    await chai
+      .request('https://adobeioruntime.net/')
+      .get(`${getbaseurl()}${qs}`)
+      .then((response) => {
+        expect(response).to.have.status(301);
+      }).catch((e) => {
+        throw e;
+      });
+  }).timeout(10000);
+
+  it('No Redirect', async () => {
+    const qs = '?owner=trieloff&repo=helix-demo&ref=4e05a4e2c7aac6dd8d5f2b6dcf05815994812d7d&path=test.md';
+    // eslint-disable-next-line no-console
+    console.log(`Trying https://adobeioruntime.net/${getbaseurl()}${qs}`);
+
+    await chai
+      .request('https://adobeioruntime.net/')
+      .get(`${getbaseurl()}${qs}`)
+      .then((response) => {
+        expect(response).to.have.status(204);
+      }).catch((e) => {
+        throw e;
+      });
+  }).timeout(10000);
+
 });
